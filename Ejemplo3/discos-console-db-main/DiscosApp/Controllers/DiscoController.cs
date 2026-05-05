4e6ad3b084eb56baf02dde23973bd0a99be342cb
Yamil <yamil_diaz@hotmail.com>
2026-05-05 02:24:06 -0300

    Initial commit

---
+++ Ejemplo3/discos-console-db-main/DiscosApp/Controllers/DiscoController.cs
@@ -0,0 +1,126 @@
+﻿using dominio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using negocio;
+
+namespace DiscosApp.Controllers
+{
+    public class DiscoController : Controller
+    {
+        // GET: DiscoController
+        public ActionResult Index()
+        {
+            DiscoNegocio negocio = new DiscoNegocio();
+            return View(negocio.listar());
+        }
+
+        // GET: DiscoController/Details/5
+        public ActionResult Details(int id)
+        {
+            DiscoNegocio discoNegocio = new DiscoNegocio();
+
+            var disco = discoNegocio.listar().Find(d => d.Id == id);
+            return View(disco);
+        }
+
+        // GET: DiscoController/Create
+        public ActionResult Create()
+        {
+            EstiloNegocio estiloNegocio = new EstiloNegocio();
+            TipoEdicionNegocio edicionNegocio = new TipoEdicionNegocio();
+
+            ViewBag.Estilo = new SelectList(estiloNegocio.listar(), "Id", "Descripcion");
+            ViewBag.TipoEdicion = new SelectList(edicionNegocio.listar(), "Id", "Descripcion");
+            return View();
+        }
+
+        // POST: DiscoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Disco disco)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(disco);
+                }
+
+                DiscoNegocio negocio = new DiscoNegocio();
+                negocio.agregar(disco);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DiscoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            EstiloNegocio estiloNegocio = new EstiloNegocio();
+            TipoEdicionNegocio edicionNegocio = new TipoEdicionNegocio();
+            DiscoNegocio discoNegocio = new DiscoNegocio();
+
+            var disco = discoNegocio.listar().Find(d => d.Id == id);
+
+            if (disco != null)
+            {
+                ViewBag.Estilo = new SelectList(estiloNegocio.listar(), "Id", "Descripcion", disco.Estilo.Id);
+                ViewBag.TipoEdicion = new SelectList(edicionNegocio.listar(), "Id", "Descripcion", disco.TipoEdicion.Id);
+            }
+           
+            return View(disco);
+        }
+
+        // POST: DiscoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Disco disco)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(disco);
+                }
+
+                DiscoNegocio discoNegocio = new DiscoNegocio();
+                discoNegocio.modificar(disco);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DiscoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            DiscoNegocio discoNegocio = new DiscoNegocio();
+
+            var disco = discoNegocio.listar().Find(d => d.Id == id);
+            return View(disco);
+        }
+
+        // POST: DiscoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                DiscoNegocio discoNegocio = new DiscoNegocio();
+                discoNegocio.eliminar(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Ejemplo3/discos-console-db-main/dominio/Disco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Disco
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El titulo es requerido")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "La fecha es requerida")]
+        [DisplayName("Fecha")]
+        public DateTime FechaLanzamiento { get; set; }
+        [Required(ErrorMessage = "La cantidad de temas es requerida")]
+        [DisplayName("Canciones")]
+        public int CantidadCanciones { get; set; }
+        [Required(ErrorMessage = "La url de la imagen es requerida")]
+        [DisplayName("Imagen")]
+        public string UrlTapa { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un estilo")]
+        public Estilo Estilo { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un tipo de edicion")]
+        public TipoEdicion TipoEdicion { get; set; }
+    }
+}
